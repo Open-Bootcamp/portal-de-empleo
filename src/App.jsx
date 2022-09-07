42df212dc3fa1533c070dd223bbaf0c7dfa32a4f
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-09-07 14:45:42 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className='App bg-slate-800 min-h-screen flex items-center justify-center'>
+			<h1 className='text-3xl font-bold text-teal-500'>Vite + React!</h1>
+		</div>
+	)
+}
+
+export default App
