ecadf913b1815b3254fce7ba08135e73774d646e
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-09-07 15:20:03 -0400

    add assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 function App() {
-	return (
-		<div className='App bg-slate-800 min-h-screen flex items-center justify-center'>
-			<h1 className='text-3xl font-bold text-teal-500'>Vite + React!</h1>
-		</div>
-	)
+	return <div className='App'></div>
 }
 
 export default App
