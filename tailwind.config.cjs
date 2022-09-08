c3c0fbd3d9a8e40e06247708bb76e2afc16edfb8
aixmarina <marina.aix.gomez@gmail.com>
2022-09-08 18:40:49 +0200

    add grid layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateRows: {
+				'layout': '120px 1fr 120px'
+			}
+		},
+		colors: {
+			'dark-cyan': '#5ba4a4',
+			'light-grayish-cyan-bg': '#effafa',
+			'light-grayish-cyan-ft': '#eef6f6',
+			'dark-grayish-cyan': '	#7b8e8e',
+			'very-dark-grayish-cyan': '#2c3a3a'
+		}
 	},
 	plugins: [],
 }
