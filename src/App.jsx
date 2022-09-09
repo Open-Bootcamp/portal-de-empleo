3d437871109d180f7be149ef91268c0203f6372c
Fabio Alcocer Sejas <fabioalcocersejas17@gmail.com>
2022-09-09 15:13:17 -0400

    Revert "Develop 003"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
-import CardsContainer from './components/CardsContainer'
-import Footer from './components/Footer'
-import Header from './components/Header'
-
 function App() {
-	return (
-		<div className='App grid grid-rows-layout text-xl'>
-			<Header />
-			<CardsContainer />
-			<Footer />
-		</div>
-	)
+	return <div className='App text-xl bg-slate-800'></div>
 }
 
 export default App
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,18 +2,7 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
-		extend: {
-			gridTemplateRows: {
-				layout: '80px 1fr 80px',
-			},
-			colors: {
-				'dark-cyan': '#5ba4a4',
-				'light-grayish-cyan-bg': '#effafa',
-				'light-grayish-cyan-ft': '#eef6f6',
-				'dark-grayish-cyan': '	#7b8e8e',
-				'very-dark-grayish-cyan': '#2c3a3a',
-			},
-		},
+		extend: {},
 	},
-	plugins: [require('prettier-plugin-tailwindcss')],
+	plugins: [],
 }
