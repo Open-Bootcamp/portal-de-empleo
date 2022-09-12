aed06ca75ed1543d6794027dac0e03f3c5a5840c
aixmarina <marina.aix.gomez@gmail.com>
2022-09-12 19:41:59 +0200

    add new component Details

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,19 @@
-import logo from '../assets/photosnap.svg'
 import CardTags from './CardTags'
 
 function Card({ job, handleTagClick }) {
 	return (
 		<div className='container mx-auto flex max-w-6xl flex-col rounded-md border-l-4 border-dark-cyan bg-white p-2 pl-4 lg:flex-row'>
 			<img
-				src={logo}
+				src={job.logo}
 				alt='logo'
 				className='-mt-7 ml-1 w-12 lg:mt-0 lg:mr-5 lg:h-fit lg:w-auto lg:self-center'
 			/>
 			<div className='flex flex-col gap-1 border-b-[1px] border-slate-500/30 text-left lg:mr-auto lg:border-none'>
 				<div className='mt-3 flex items-center gap-2'>
 					<h3 className='mr-5 mt-1 text-lg font-bold text-dark-cyan lg:text-xl'>
-						{job.company}
+						{job.company.name}
 					</h3>
-					{job.new && (
+					{job.isRecent && (
 						<p className='rounded-full bg-dark-cyan px-2 pt-1 text-sm font-bold uppercase text-white'>
 							New!
 						</p>
