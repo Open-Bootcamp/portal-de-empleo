6216ba73403c3c76ff5760199afb71448d138df8
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-09-12 17:04:45 -0400

    add loader to details section

--- src/components/CardTags.jsx
+++ src/components/CardTags.jsx
@@ -16,7 +16,7 @@ function CardTags({ job, handleTagClick }) {
 				<span
 					key={uuidv4()}
 					onClick={() => handleTagClick(tag)}
-					className='cursor-pointer rounded-sm bg-light-grayish-cyan-ft px-2 py-1 text-sm font-bold text-dark-cyan transition-colors duration-300 hover:bg-dark-cyan hover:text-light-grayish-cyan-ft'
+					className='cursor-pointer rounded-md bg-light-grayish-cyan-ft px-2 py-2 pb-1 text-base font-bold text-dark-cyan transition-colors duration-300 hover:bg-dark-cyan hover:text-light-grayish-cyan-ft'
 				>
 					{tag}
 				</span>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { FaInstagram, FaTwitch, FaTwitter } from 'react-icons/fa'
 
 function Footer() {
 	return (
-		<div className='flex h-24 items-center justify-center bg-dark-cyan p-3 text-center text-lg text-[#FFFFFF] drop-shadow-[0_-4px_4px_rgba(0,0,0,0.20)]'>
+		<div className='flex items-center justify-center bg-dark-cyan p-3 text-center text-lg text-[#FFFFFF] drop-shadow-[0_-4px_4px_rgba(0,0,0,0.20)]'>
 			<div className='text-center'>
 				<span className=' text-sm md:text-lg'>
 					© OpenBootCamp - Open WeekApps - 2022
